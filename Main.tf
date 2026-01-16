ed6dcd44483f89efe9fccf3d4b23ec75d16edb1d
Mahammed Shadman Ahmed <mshadmana@gmail.com>
2026-01-16 11:05:51 +0000

    update

--- Main.tf
+++ Main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraformseptbucketsun13" {
+  bucket = "terraformseptbucketsun13"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraformseptbucketsun13"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
