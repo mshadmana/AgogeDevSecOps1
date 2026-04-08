0135f067271d77d02a9dce0e75e6b96f113463b7
Mahammed Shadman Ahmed <mshadmana@gmail.com>
2026-04-08 20:01:06 +0100

    update

--- Main.tf
+++ Main.tf
@@ -5,10 +5,10 @@ resource "aws_s3_bucket" "terraformseptbucketsun13" {
   bucket = "terraformseptbucketsun13"
 }
 terraform {
-  backend "s3" {
-    # Replace this with your bucket name!
-    bucket         = "terraformseptbucketsun13"
-    key            = "global/s3/terraform.tfstate"
-    region         = "eu-west-2"
+  backend "s3"{
+    bucket ="terraformseptbucketsun13"
+    key = "global/s3/terraform.tfstate"
+    region = "eu-west-2"
   }
+  
 }
